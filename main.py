38a72abf49253747a8c83da5716b10ae3ab6fd2b
Rushi Machale <98655276+rushikeshmachale@users.noreply.github.com>
2023-03-08 17:19:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask,request,jsonify
 
+
+# import pandas as pd
+import numpy as np
+
+
+import pickle
 app = Flask(__name__)
+lrmodel = pickle.load(open('models','rb'))
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def hom():
+    return "ha pn chalu kay"
+
+@app.route('/find',methods=['POST'])
+def main():
+    Marks = request.form.get('Marks')
+    Stamina = request.form.get('Stamina')
+    Present = request.form.get('Present')
+    Absent = request.form.get('Absent')
+    Avg = request.form.get('Avg')
+    Score = request.form.get('Score')
+    Project = request.form.get('Project')
+    Practical = request.form.get('Practical')
+    Internship = request.form.get('Internship')
+    Unit = request.form.get('Unit')
+
+    input_query = np.array([[Marks,Stamina,Present,Absent,Avg,Score,Project,Practical,Internship,Unit]],dtype=float)
+    result = lrmodel.predict(input_query)[0]
+    return jsonify({'Status': str(result)})
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
